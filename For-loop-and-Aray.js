6657bc61f50c3edd0c9d24d1954365a142e2585d
mobarizjalalzai <mobariz.jalalzai@gmail.com>
2024-09-05 09:29:38 +0430

    For-loop-and- Aray
    this is my home work

---
+++ For-loop-and-Aray.js
@@ -0,0 +1,17 @@
+
+
+var CarModels= ['crolla','4thRunnr', 'Laxis', 'Land cruser','TOYOTA', 'BMW'];
+                //0         1           2           3           4          5
+//n-1
+//5-1=4
+//for ('initialization'; 'condition'; 'increment'; 'decrement';)
+
+for(i=0;i<CarModels.length;i++){
+    console.log(CarModels[i]);
+                                //carModels[0]
+                                //carModels[1]
+                                //carModels[2]
+                                //carModels[3]
+                                //carModels[4]
+                                //carModels[5]
+}
